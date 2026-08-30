8497680548ad539451423f387559c8d49b6b8379
Lincy Bainiwal <lincy.bainiwal21@ibm.com>
2026-08-30 21:54:09 +0530

    remove provider block

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@
 terraform {
   required_version = ">= 1.15.0"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = "~> 5.0"
+  #   }
+  # }
 
   cloud {
     
