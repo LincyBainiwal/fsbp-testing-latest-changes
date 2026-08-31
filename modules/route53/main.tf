aca5d4426dd0a30548fe25604af1d0bd0237d1bf
Lincy Bainiwal <lincy.bainiwal21@ibm.com>
2026-08-31 15:20:57 +0530

    add provider

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 5.0"
+      version               = ">= 5.0"
       configuration_aliases = [aws.us_east_1]
     }
     # Route53 query logs MUST be delivered to CloudWatch Logs in us-east-1
